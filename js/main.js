eb49ace920a97cad2c593f8e54fcfe441e25f249
Elisa Campos <elisacampospousa@gmail.com>
2017-05-30 15:04:07 +0200

    Add JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function appearMenu(){
+  var showMenu = document.querySelector(".menu-mobile");
+  showMenu.classList.toggle("active");
+}
+
+var menubtn = document.querySelector(".btn-menu-mobile");
+ menubtn.addEventListener("click", appearMenu); 
